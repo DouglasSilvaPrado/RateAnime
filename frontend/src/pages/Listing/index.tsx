83bb5af91c10cece243ff8f49ef44591442da016
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-03-12 14:34:07 -0300

    styles

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -22,7 +22,7 @@ function Listing() {
   })
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+    axios.get(`${BASE_URL}/movies?size=9&page=${pageNumber}&sort=id`)
     .then(response => {
       const data = response.data as MoviePage
       setPage(data)
@@ -48,6 +48,7 @@ function Listing() {
                     
         </div>
       </div>
+      <Pagination page={page} onChange={handlePageChange} />
     </>
   );
 }
